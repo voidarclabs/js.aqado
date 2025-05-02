7896b82c902213c55cee402a347b15258d495f37
voidarclabs <dominicbourne77@gmail.com>
2025-05-02 12:31:03 +0100

    Basically finished

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const socketio = require("socket.io");
 
 // Serve the index.html file when the root path is requested
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Start the server
-app.listen(100, () => {
-  console.log('Server listening on port 100');
+const server = app.listen(9000, () => {
+  console.log("Server listening on port 9000");
+});
+
+// declare websocket server
+const io = socketio(server);
+
+const functions = {
+  test: { func: test },
+};
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("clientUpdate", (data) => {
+    if (functions[data[0]]) {
+      functions[data[0]].func(data[1]);
+    }
+  });
 });
